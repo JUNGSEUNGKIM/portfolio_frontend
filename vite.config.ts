1c19f23fc405f5190a1d658f078faf46c06dea9d
JUNGSEUNGKIM <153578440+JUNGSEUNGKIM@users.noreply.github.com>
2025-04-23 09:50:59 +0900

    feat: Docker setting test

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: ['test.arami.kr'], // 여기에 허용할 도메인 추가!
+  },
 })
