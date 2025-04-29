91f22b13e6f309c1bb4af674b89695b6034912c1
JUNGSEUNGKIM <153578440+JUNGSEUNGKIM@users.noreply.github.com>
2025-04-29 09:52:39 +0900

    fix: build error fixed

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import  AramiMain  from "@/pages/AramiMain.tsx";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Body from "@/components/Body.tsx";
+// import Body from "@/components/Body.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
